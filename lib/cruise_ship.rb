400d61e11e0ff07be5e76aef3892cad06d8a9eff
Caroline Berg <carolineeberg@gmail.com>
2018-08-10 15:14:49 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,3 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
 
 def select_winner(passengers)
   passengers = {
@@ -15,3 +8,12 @@ def select_winner(passengers)
   suite_e: "Crumpet the Elf"
   }
 end
+
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner  
